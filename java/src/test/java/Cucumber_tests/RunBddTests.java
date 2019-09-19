19dbf333846fd0495c793d79b2c39a245afe8b35
Tomasz Dworowy <tdworowy@sii.pl>
2019-09-19 14:17:11 +0200

    update

--- java/src/test/java/Cucumber_tests/RunBddTests.java
+++ java/src/test/java/Cucumber_tests/RunBddTests.java
@@ -2,12 +2,11 @@ package Cucumber_tests;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/java/Features"},
+@CucumberOptions(features={"src/test/resources/features"},
         glue={"Steps"},
         plugin = { "pretty","html:target/cucumber-reports" })
 public class RunBddTests {
